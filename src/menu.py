fe1c455b13c0f5918bf44dd22a8ce2db9dcb44a4
York M <YorkM@genstudents.org>
2024-10-28 17:24:08 +0000

    refactor csvfile methods

--- src/menu.py
+++ src/menu.py
@@ -17,6 +17,12 @@ class Menu(ABC):
             f"4. Delete {self.context}\n"
             f"0. Return to the main menu"
         )
+    
+    def validate_user_selection(self, user_selection: int, function, *args):
+        if user_selection:
+            function(user_selection, *args)
+            return True
+        return False
 
     @print_buffer_exit
     def get_user_selection(self) -> int:
@@ -85,12 +91,6 @@ class StringListMenu(Menu):
     def delete_element(self, user_selection: int) -> None:
         print(f"{self.user_list.pop(user_selection - 1)} has been deleted!")
 
-    def validate_user_selection(self, user_selection: int, function, *args):
-        if user_selection:
-            function(user_selection, *args)
-            return True
-        return False
-
     def menu_choice(self) -> bool:
         user_input = get_input("Please enter a number to select your menu choice:\n> ",'int')
         print_buffer()
@@ -112,38 +112,95 @@ class StringListMenu(Menu):
 
 class CSVListMenu(Menu):
 
-    def __init__(self, context: str, input_list: list[dict], template: dict):
+    def __init__(self, context: str, input_list: list[dict], template: dict = None):
         super().__init__(context, input_list)
-        self.template = template
+        if template is None:
+            # Make the template
+            self.template = None
+        else:
+            self.template = template
     
     def clean_key(self, input: str) -> str:
         return input.replace('_', ' ').capitalize()
     
     def print_dict(self, dictionary: dict):
         for key, item in dictionary.items():
-            print(f"{self.clean_key(key)}: {item}")
-    
+            match key:
+                case key if 'address' in key:
+                    print(f"{self.clean_key(key): {self.address_decoder(item)}}")
+                case _:
+                    print(f"{self.clean_key(key)}: {item}")
+
     def print_list(self):
         for index, element in enumerate(self.user_list):
             print(f"{index+1}.")
             self.print_dict(element)
     
-    def add(self):
-        return super().add()
+    def add(self,):
+        new_dict = {}
+        for key in self.template:
+            new_dict[key] = get_input(f"Please enter your {self.clean_key(key)}:\n> ")
+        if any(filter(lambda key: 'address' in key, new_dict.keys())):
+            # This does not work
+            # Find property and then update it
+            for _property in new_dict:
+                if 'address' in _property:
+                    new_dict[_property] = self.address_encoder(new_dict[_property])
+                    break
+        self.user_list.append(new_dict)
     
     def address_encoder(address: str) -> str:
-        return address.replace(',', '_')
+        return address.replace(',', '|')
 
     def address_decoder(address: str) -> str:
         return address.replace('|', ',')
     
+    def get_property(self,):
+        key_match = get_input("Enter which property you would like to select:\n> ")
+        while not any(filter(lambda key: key_match.lower() in key, self.template.keys())):
+            key_match = get_input("Enter which property you would like to select:\n> ")
+        return key_match
+    
+    def get_new_property_value(self, property_context):
+        print(f"Enter new {property_context}:\n")
+        return get_input("> ")
+    
     def update(self, user_selection: int, property_selected: str, updated_property: str):
-        selected_dict = user_selection - 1
-        user_selection[property_selected] = updated_property
+        selected_dict = self.user_list[user_selection - 1]
+        selected_dict[property_selected] = updated_property
     
     def delete_element(self, user_selection: int):
-        print(f"{self.user_list.pop(user_selection - 1)} has been deleted!")
+        removed_element = self.user_list.pop(user_selection - 1)
+        self.print_dict(removed_element)
+        print(f"\nHas been deleted!")
     
     def menu_choice(self):
-        return super().menu_choice()
+        user_input = get_input("Please enter a number to select your menu choice:\n> ",'int')
+        print_buffer()
+        match user_input:
+            case 1:
+                self.print_list()
+            case 2:
+                self.add()
+            case 3:
+                self.validate_user_selection(
+                    self.get_user_selection(), 
+                    self.update, 
+                    self.get_property(),
+                    self.get_new_property_value()
+                    )
+                self.update()
+            case 4:
+                self.validate_user_selection(
+                    self.get_user_selection(), 
+                    self.delete_element
+                    )
+            case 0:
+                # Apply encoder to all addresses
+                return False
+            case _:
+                print("Please select a valid option")
+                print_buffer()
+
+        return True
     
