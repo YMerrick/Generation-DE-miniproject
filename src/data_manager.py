305a54cb5eb6ba68bf77c5014c4ffd4a3abe8772
York M <YorkM@genstudents.org>
2024-11-08 11:41:44 +0000

    add selection methods to data manager

--- src/data_manager.py
+++ src/data_manager.py
@@ -41,14 +41,33 @@ class DictDataManager(DataManagerInterface):
     def __init__(self, input_list: list[dict]):
             super().__init__()
             self.user_list = input_list
-
-    def __str__(self):
+    
+    def get_in_tabular_form(self) -> str:
         return tabulate(self.user_list, 
                         headers= {k: k.replace('_',' ').capitalize() for k in self.get_keys()}, 
                         showindex=(index + 1 for index in range(self.get_length())),
                         floatfmt='.2f',
                         tablefmt='rounded_grid'
                         )
+
+    # Only return selected columns
+    def select_columns(self, *cols: list[str]) -> list[dict] | None:
+        for col in cols:
+            if not col in self.get_keys():
+                return None
+
+        new_list = []
+        for entry in self.user_list:
+            new_list.append({key: entry[key] for key in cols})
+
+        return new_list
+
+    # Return entries matching a search term in specified column
+    def filter_on_column(self, col: str, search_term: str) -> list[dict] | None:
+        if not col in self.get_keys():
+            return None
+
+        return [entry for entry in self.user_list if search_term.lower() in entry[col].lower()]
 
     def add(self, new_dict):
         self.user_list.append(new_dict)
