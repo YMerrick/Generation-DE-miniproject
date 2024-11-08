09709490675e60d88c2020d53548db89a748a242
York M <YorkM@genstudents.org>
2024-11-08 14:37:43 +0000

    remove obsolete code

--- src/file_handler.py
+++ src/file_handler.py
@@ -18,59 +18,6 @@ class MyFileHandler(DataHandler):
     def __init__(self, filepath: str):
         super().__init__()
         self.filename: str = filepath
-
-class TextFile(MyFileHandler):
-
-    def __init__(self,filepath, *, mode = 'r+t', **kwargs):
-        super().__init__(filepath)
-        self.__file = None
-        self.__mode = mode
-        self.__kwargs = kwargs
-
-    def __enter__(self):
-        try:
-            self.__file = open(self.filename, self.__mode, **self.__kwargs)
-        except FileNotFoundError:
-            Path("data/").mkdir(parents=True, exist_ok=True)
-            self.__file = open(self.filename, 'x+t', **self.__kwargs)
-        except TypeError:
-            Path("data/").mkdir(parents=True, exist_ok=True)
-            self.__file = open(f"data/{__name__}.txt", 'w+t', **self.__kwargs)
-        except Exception as ex:
-            print(ex)
-        return self
-
-    def __exit__(self, *args):
-        self.__file.close()
-
-    def __del__(self):
-        if self.__file:
-            self.__file.close()
-
-    # Reads from file and then return list of products
-    def load(self) -> list[str]:
-        if self.__file is None:
-            with self as fh:
-                return fh.load()
-        
-        return [line.rstrip() for line in self.__file if line.rstrip()]
-        
-    # Saves from list to file
-    def save(self,input_list: list[str]) -> bool:
-        if self.__file is None:
-            with self as fh:
-                return fh.save(input_list)
-        
-        was_closed = self.__file.closed
-        if was_closed:
-            self.__file = open(self.filename, 'wt')
-
-        self.__file.write('\n'.join(input_list))
-
-        if was_closed:
-            self.__file.close()
-
-        return True
     
 class CSVFile(MyFileHandler):
 
